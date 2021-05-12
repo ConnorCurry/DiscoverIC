86128f040e498950c4f266bf5fcb2a773fd34c85
ConnorCurry <connor4424@gmail.com>
2021-05-12 19:29:24 -0400

    moved building info to own file in components

---
+++ Components/BuildingInfo.js
@@ -0,0 +1,34 @@
+export const buildingInfo = {
+    "James J. Whalen Center for Music" : {
+        summary: "The James J. Whalen Center for Music houses Ithaca College's Music School. It contains stunning performance spaces"+
+            " like Ford Concert Hall and The Hockett Family Recital hall, as well as several large rehearsal spaces, classrooms, offices, and practice rooms." +
+            "",
+        studies: "Music Majors",
+        studentQuotes: [
+            {key: "1", quote: "Diva Lounge is great for socializing, but McHenry has a brilliant view and is usually a bit quieter and better for working."},
+            {key: "2", quote: "Hockett hall has fantastic acoustics for solo performances."},
+        ],
+        accessible: "Yes",
+        images: [
+            {text: "Practice Room 1321", image: require('../assets/building-images/jjwcm/1321.jpeg')},
+            {text: "Practice Room 1322", image: require('../assets/building-images/jjwcm/1322.jpeg')},
+            {text: "Practice Room 1343", image: require('../assets/building-images/jjwcm/1343.jpeg')},
+            {text: "Practice Room 1347", image: require('../assets/building-images/jjwcm/1347.jpeg')},
+            {text: "Classroom 2328", image: require('../assets/building-images/jjwcm/2328.jpeg')},
+            {text: "Rehearsal Room and Classroom 3302", image: require('../assets/building-images/jjwcm/3302.jpeg')},
+            {text: "Rehearsal Room and Classroom 3304", image: require('../assets/building-images/jjwcm/3304.jpeg')},
+            {text: "Fourth Floor Atrium", image: require('../assets/building-images/jjwcm/atrium.jpeg')},
+            {text: "Diva Lounge", image: require('../assets/building-images/jjwcm/diva.jpeg')},
+            {text: "Ford Concert Hall", image: require('../assets/building-images/jjwcm/ford.jpeg')},
+            {text: "Husa Gallery", image: require('../assets/building-images/jjwcm/husa.jpeg')},
+            {text: "Iger Hall", image: require('../assets/building-images/jjwcm/iger.jpeg')},
+            {text: "McHenry Lounge", image: require('../assets/building-images/jjwcm/mchenry.jpeg')},
+            {text: "Presser Rehearsal Hall", image: require('../assets/building-images/jjwcm/presser1.jpeg')},
+            {text: "Presser Rehearsal Hall", image: require('../assets/building-images/jjwcm/presser2.jpeg')},
+            {text: "Instrument Repair Window", image: require('../assets/building-images/jjwcm/repair-window.jpeg')},
+        ]
+    },
+    "Dillingham Center" : {
+        summary: ""
+    },
+}
